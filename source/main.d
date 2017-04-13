4c828883089d8820143c5c290493993b1b26fe0c
dd <devddstuff@gmail.com>
2017-04-12 21:30:32 -0400

    getopt megatweak

--- source/main.d
+++ source/main.d
@@ -17,69 +17,68 @@ else
         rt_envvars_enabled = false, rt_cmdline_enabled = false;
 }
 
-string[] args;
-
-int main(string[] args_)
+int main(string[] args)
 {
-    args = args_;
-
     bool cont; // Continue with symlink, disable by default
-    bool shwVersion;
+
+    if (args.length <= 1)
+    {
+        print_help;
+        return 0;
+    }
 
     import std.getopt;
-    GetoptResult rslt;
+    GetoptResult r;
 	try {
-		rslt = getopt(args,
-            "base10|b", "", &Base10,
+		r = getopt(args,
+            config.bundling,
+            "base10|b", "Use decimal metrics instead of binary", &Base10,
+            config.bundling,
             "continue|c", "Continue on soft symlink", &cont,
+            config.bundling,
 			"more|m", "Print more information if available", &More,
+            config.bundling,
 			"showname|s", "Show filename before result", &ShowingName,
-            "version|v", "Print version information", &shwVersion);
-	} catch(GetOptException e) {
-        import core.stdc.stdlib : exit;
-		stderr.writefln("%s", e.msg);
-        exit(1);
+            "version|v", "Print version information", &print_version);
+	} catch (GetOptException ex) {
+		stderr.writeln(ex.msg);
+        return 1;
 	}
 
-    if(args_.length <= 1 || rslt.helpWanted) {
-        writeln("Determine the file type by its content.");
-		writefln("  Usage: %s [<Options>] <File>", PROJECT_NAME);
-        writefln("         %s {-h|--help|-v|--version|/?}", PROJECT_NAME);
-        if(rslt.helpWanted)
-    	{
-    		defaultGetoptPrinter("\nSome information about the program.",
-    		rslt.options);
-        }
-	} else if (shwVersion) {
-        debug
-            writefln("%s %s -debug (%s)", PROJECT_NAME, PROJECT_VERSION, __TIMESTAMP__);
-        else
-            writefln("%s %s (%s)", PROJECT_NAME, PROJECT_VERSION, __TIMESTAMP__);
-            writeln("MIT License: Copyright (c) 2016-2017 dd86k");
-            writeln("Project page: <https://github.com/dd86k/dfile>");
-            writefln("Compiled %s with %s v%s", __FILE__, __VENDOR__, __VERSION__);
-	} else {
-        string filename = args[1]; // Last argument, no exceptions!
+    if (r.helpWanted)
+    {
+        print_help;
+        defaultGetoptPrinter("\nSwitches", r.options);
+        /*foreach (it; r.options)
+        { // "custom" defaultGetoptPrinter
+            writefln("%*s, %*s%s    %s",
+                4,  it.optShort,
+                10, it.optLong,
+                it.required ? " Required: " : " ",
+                it.help);
+        }*/
+	}
+    else
+    {
+        string filename = args[$ - 1]; // Last argument, no exceptions!
 
         if (exists(filename)) {
             prescan(filename, cont);
-        }
-        else
-        {
+        } else {
             import std.string : indexOf;
-            bool glob =
-                indexOf(filename, '*', 0) >= 0 || indexOf(filename, '?', 0) >= 0 ||
-                indexOf(filename, '[', 0) >= 0 || indexOf(filename, ']', 0) >= 0;
-            if (glob) { // No point to do globbing if there are no metacharacters
+            // No point to do globbing if there are no metacharacters
+            if (indexOf(filename, '*', 0) >= 0 || indexOf(filename, '?', 0) >= 0 ||
+                indexOf(filename, '[', 0) >= 0 || indexOf(filename, ']', 0) >= 0) { 
                 import std.path : globMatch, dirName;
                 debug writeln("GLOB ON");
                 ShowingName = !ShowingName;
                 int nbf;
-                foreach (DirEntry dir; dirEntries(dirName(filename), SpanMode.shallow, cont))
+                foreach (DirEntry e;
+                    dirEntries(dirName(filename), SpanMode.shallow, cont))
                 {
-                    if (globMatch(dir.name, filename)) {
+                    if (globMatch(e.name, filename)) {
                         ++nbf;
-                        prescan(dir.name, cont);
+                        prescan(e.name, cont);
                     }
                 }
                 if (!nbf)
@@ -90,7 +89,7 @@ int main(string[] args_)
             }
             else // No glob!
             {
-    F_NE:
+F_NE:
                 report("File does not exist");
                 return 1;
             }
@@ -113,4 +112,24 @@ void prescan(string filename, bool cont)
         report_dir(filename);
     else
         report_unknown(filename);
+}
+
+void print_help()
+{
+    writeln("Determine the file type by its content.");
+    writefln("  Usage: %s [<Options>] <File>", PROJECT_NAME);
+    writefln("         %s {-h|--help|-v|--version|/?}", PROJECT_NAME);
+}
+
+void print_version()
+{
+    import core.stdc.stdlib : exit;
+    debug
+        writefln("%s %s-debug (%s)", PROJECT_NAME, PROJECT_VERSION, __TIMESTAMP__);
+    else
+        writefln("%s %s (%s)", PROJECT_NAME, PROJECT_VERSION, __TIMESTAMP__);
+    writeln("MIT License: Copyright (c) 2016-2017 dd86k");
+    writeln("Project page: <https://github.com/dd86k/dfile>");
+    writefln("Compiled %s with %s v%s", __FILE__, __VENDOR__, __VERSION__);
+    exit(0); // Due to getopt, hack
 }
